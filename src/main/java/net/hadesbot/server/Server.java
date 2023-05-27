ffa382cbe83da18c47ecec0c21aed4efc45d5a79
ziuee <remig06@hotmail.com>
2023-05-27 23:19:58 +0200

    Create workspace

---
+++ src/main/java/net/hadesbot/server/Server.java
@@ -0,0 +1,25 @@
+package net.hadesbot.server;
+
+/**
+ * The server class responsible for handling server operations.
+ *
+ * @author Hades Development
+ * @project Hades
+ * @date 5/27/2023
+ */
+public class Server {
+
+    /**
+     * Constructs a new instance of the Server class.
+     */
+    public Server() {
+
+    }
+
+    /**
+     * Starts the server.
+     */
+    public void start() {
+
+    }
+}
---
+++ src/main/java/net/hadesbot/server/launch/Launch.java
@@ -0,0 +1,21 @@
+package net.hadesbot.server.launch;
+
+import net.hadesbot.server.Server;
+
+/**
+ * The main class responsible for launching the Hades server.
+ *
+ * @author Hades Development
+ * @project Hades
+ * @date 5/27/2023
+ */
+public class Launch {
+
+    public static void main(final String[] args) {
+        // Set the file encoding to UTF-8
+        System.setProperty("file.encoding", "UTF-8");
+
+        // Start the server
+        new Server().start();
+    }
+}
